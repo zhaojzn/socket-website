39c675debd8eb49188335fedf9a3d8e23c431326
Jason Zhao <jasonnope123@gmail.com>
2023-05-24 11:30:48 -0700

    page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+        <div className='h-3/5 w-1/2 bg-black rounded-lg flex flex-col items-center'>
+            <p>Test</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
