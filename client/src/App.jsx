6a5ea2fe9f86e3b83bea5d2c33f4f9d77c356f75
zjzn <jasonnope123@gmail.com>
2023-05-24 00:41:42 -0700

    gamble 50/50

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
 import io from 'socket.io-client'
-const socket = io('http://localhost:3000')
-function App() {
 
+function App() {
+  const [balance, setBalance] = useState(0);
+  const [userId, setUserId] = useState('');
+  let socket;
+  useEffect(() => {
+    socket = io('http://localhost:3000')
+
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    // Event listener for receiving gamble result
+    socket.on('gambleResult', (result) => {
+      console.log('Received gamble result:', result);
+      // Update the relevant component with the received result
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const serverCall = () => {
+    const betAmount = 100; // Replace with the actual bet amount from your component
+    socket.emit('placeBet', betAmount);
+  };
 
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="h-1/2 w-1/2 items-center justify-center text-center flex flex-col">
-        <span>Balance: 0</span>
-        <span>User: ID goes here</span>
-
+        <span>Balance: {balance}</span>
+        <span>User: {userId}</span>
+        <button onClick={serverCall}>Click here</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App
--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 app.use(cors());
 const server = http.createServer(app);
 
+const gambleHistory = [];
+
 const io = new Server(server, {
     cors: {
         origin: '*',
@@ -14,10 +16,30 @@ const io = new Server(server, {
 });
 
 
-io.on("connection", (socket) => {
-    console.log("a user connected");
-})
-
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    // Event listener for placing a bet
+    socket.on('placeBet', (betAmount) => {
+      // Generate a random outcome (example logic)
+      const outcome = Math.random() < 0.5 ? 'win' : 'lose';
+      const result = {
+        betAmount,
+        outcome,
+      };
+  
+      // Add the result to gamble history
+      gambleHistory.push(result);
+  
+      // Emit the result back to the client
+      socket.emit('gambleResult', result);
+    });
+  
+    // Event listener for disconnecting
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
